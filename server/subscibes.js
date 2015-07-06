ad85ee0f59d3ee88c174f0f68d83eba200b4590a
Mikhail Baranov <mikhail.baranov@gmail.com>
2015-07-07 01:11:16 +0300

    Оптимизация запросов к серверу

--- server/subscibes.js
+++ server/subscibes.js
@@ -1,3 +1,7 @@
-Meteor.publish('groups', function() {
-    return Groups.find();
+Meteor.publish('allGroups', function() {
+    return Groups.find({}, {sort: {faculty: 1, title: 1}});
+});
+
+Meteor.publish('abiturients', function(groupId) {
+    return Abiturients.find({groupId: groupId});
 });
