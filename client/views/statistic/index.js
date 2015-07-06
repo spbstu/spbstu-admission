162df910b7018471a4793fbc7444eaf3c4cd82b7
Mikhail Baranov <mikhail.baranov@gmail.com>
2015-07-07 00:35:29 +0300

    Форматирование и стили для таблицы плана приёма

--- client/views/statistic/index.js
+++ client/views/statistic/index.js
@@ -3,26 +3,28 @@ Session.setDefault('groupsQuery', {});
 Template.Statistic.helpers({
     rows: function() {
         var filter = Session.get('groupsQuery'),
-            groups = Groups.find(filter),
+            groups = Groups.find(filter, {sort: {faculty: 1}}),
             prevFaculty = '';
 
-        return groups.map(function(row) {
-            var result = row;
+        return groups
+            .map(function(row) {
+                var result = row;
 
-            if (row.faculty !== prevFaculty) {
-                prevFaculty = row.faculty;
-                result.isFirst = true;
-            } else {
-                result.faculty = '';
-            }
+                if (row.faculty !== prevFaculty) {
+                    prevFaculty = row.faculty;
+                    result.isFirst = true;
+                } else {
+                    result.faculty = '';
+                }
+                result.isActive = row.applicationsCount > 0;
 
-            return result;
-        });
+                return result;
+            });
     }
 });
 
 Template.Statistic.events({
-    'click .data-row': function(e) {
+    'click .data-row_active': function(e) {
         var groupId = e.currentTarget.dataset['groupid'];
 
         Router.go('group', {groupId: groupId});
