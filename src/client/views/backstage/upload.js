16c0ad021b105b51d41499a8595d868201cfd838
Mikhail Baranov <mikhail.baranov@gmail.com>
2015-07-15 01:09:52 +0300

    Improve file fields behavior

--- src/client/views/backstage/upload.js
+++ src/client/views/backstage/upload.js
@@ -7,12 +7,12 @@ Template.BackstageUpload.events({
             if (err) {
                 console.log('Upload error:', err);
             } else {
-
+                template.find('.upload-form').reset();
             }
         });
     },
 
-    'change #counters': function(event) {
+    'change #counters': function(event, template) {
         var input = event.target,
             file = event.target.files[0];
 
@@ -20,12 +20,12 @@ Template.BackstageUpload.events({
             if (err) {
                 console.log('Upload error:', err);
             } else {
-
+                template.find('.upload-form').reset();
             }
         });
     },
 
-    'change #abiturients': function(event) {
+    'change #abiturients': function(event,template) {
         var input = event.target,
             file = event.target.files[0];
 
@@ -33,8 +33,24 @@ Template.BackstageUpload.events({
             if (err) {
                 console.log('Upload error:', err);
             } else {
-
+                template.find('.upload-form').reset();
             }
         });
     }
 });
+
+Template.BackstageUpload.rendered = function() {
+    $('.file-field').each(function() {
+        var path_input = $(this).find('input.file-path');
+
+        $(this).find('input[type="file"]').change(function () {
+            var files = $(this)[0].files;
+            var file_names = [];
+            for (var i=0; i < files.length; i++) {
+                file_names.push(files[i].name);
+            }
+            path_input.val(file_names.join(", "));
+            path_input.trigger('change');
+        });
+    });
+};
