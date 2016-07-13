e6834034fc24a8a1be71366bb3bf77d6090eb87f
Scorik Egor <shirofaii@gmail.com>
2016-07-13 15:41:54 +0300

    remove unused Ratings collection

--- src/lib/collection.js
+++ src/lib/collection.js
@@ -1,6 +1,5 @@
 Groups = new Mongo.Collection('groups');
 Abiturients = new Mongo.Collection('abiturients');
-Ratings = new Mongo.Collection('ratings');
 
 uploadStatus = new Mongo.Collection('uploadStatus');
 SiteSettings = new Mongo.Collection('siteSettings');
@@ -29,9 +28,6 @@ Groups.deny(denyBase);
 Abiturients.allow(allowBase);
 Abiturients.deny(denyBase);
 
-Ratings.allow(allowBase);
-Ratings.deny(denyBase);
-
 uploadStatus.deny(denyBase);
 uploadStatus.allow(allowBase);
 
@@ -43,13 +39,7 @@ FSStore = new FS.Store.FileSystem("files");
 GroupsFiles = new FS.Collection("groupsFiles", {
     stores: [FSStore]
 });
-RatingGroupsFiles = new FS.Collection("ratingGroupsFiles", {
-    stores: [FSStore]
-});
 AbiturientsFiles = new FS.Collection("abiturientsFiles", {
-    stores: [FSStore]
-});
-RatingFiles = new FS.Collection("ratingFiles", {
     stores: [FSStore]
 });
 
--- src/lib/router.js
+++ src/lib/router.js
@@ -57,7 +57,6 @@ Router.map(function() {
         },
         waitOn: function() {
             return [
-                Meteor.subscribe('ratings', this.params.groupId),
                 Meteor.subscribe('groupInfo', this.params.groupId),
                 Meteor.subscribe('abiturients', this.params.groupId)
                 ];
