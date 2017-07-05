7bedf7092db131b645379147442d1b0505a0327a
masha-masha <mary_grecova@mail.ru>
2017-07-05 12:12:51 +0300

    Add files via upload

--- src/client/views/group/mainCampaignGroup.js
+++ src/client/views/group/mainCampaignGroup.js
@@ -1,6 +1,6 @@
 Template.MainCampaignGroup.helpers({
     personExamWarn: function(examType) {
-        if(examType === 'Без в/и (диплом не подтвержден ФИС)') {
+        if(examType === 'без в/и*') {
             return 'warn z-depth-1 red darken-4'
         }
     },
